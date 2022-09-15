155c45822b28d694e1ac111a5a6997aad627df05
KanishkaNaik <kanishkabc123@gmail.com>
2022-09-15 23:26:04 +0530

    35. Add HttpClientTestingModule to PostService for Resolving HttpClient Dependency - Angular testing.

---
+++ src/app/Services/Posts/http-postservice.spec.ts
@@ -0,0 +1,42 @@
+import { HttpClientTestingModule, HttpTestingController } from "@angular/common/http/testing"
+import { TestBed } from "@angular/core/testing"
+import { PostService } from "./posts.service"
+
+describe('Post Service', () => {
+    let postService: PostService;
+    let httpTestingController: HttpTestingController;
+    let POSTS = [
+        {
+            id: 1,
+            body: 'body 1',
+            title: 'title 1',
+        },
+        {
+            id: 2,
+            body: 'body 2',
+            title: 'title 2',
+        },
+        {
+            id: 3,
+            body: 'body 3',
+            title: 'title 3',
+        },
+    ];
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            providers: [PostService],
+            imports: [HttpClientTestingModule],
+        })
+        postService = TestBed.inject(PostService);
+        httpTestingController = TestBed.inject(HttpTestingController);
+    })
+    describe('getPost()', () => {
+        it('Should return  posts when getPosts() is Called', (done:DoneFn) => {
+            postService.getPosts().subscribe((data)=>{expect(data).toEqual(POSTS);done();});
+            const request = httpTestingController.expectOne('https://jsonplaceholder.typicode.com/posts');
+            request.flush(POSTS);
+            expect(request.request.method).toBe('GET');
+        })
+    })
+})
